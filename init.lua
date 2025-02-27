c725539090a61f548e87b5bc7bdd717585e4d777
jk <>
2025-02-27 21:00:36 +0100

    Added some keymaps

--- init.lua
+++ init.lua
@@ -201,6 +201,7 @@ end)
 vim.keymap.set('i', '<C-S-;>', '<Esc>A;<Enter>')
 vim.keymap.set('i', '<C-S-.>', '<Esc>A -> ')
 vim.keymap.set('i', '<C-S-,>', '<Esc>A,<Enter>')
+vim.keymap.set('i', '<C-S-l>', '<Esc>A')
 
 -- TIP: Disable arrow keys in normal mode
 vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
